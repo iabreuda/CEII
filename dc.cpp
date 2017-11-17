96be1c9c250c1ad52ef56f7a2316a407debd7609
Igor Abreu <igor.abreu.da.silva@cern.ch>
2017-11-17 00:34:37 -0300

    Criar modelo para fontes DC

---
+++ dc.cpp
@@ -0,0 +1,33 @@
+#ifndef DC
+#define DC
+/**
+ * Modelo basico de componentes
+ */
+#include "fonteindependente.cpp"
+
+/* Necessario para nao precisar escrever std:: */
+using namespace std;
+
+class DC : public FonteIndependente
+{
+    public:
+        /**
+         * Construtor
+         */
+        DC(string n, int a, int b, double v) : FonteIndependente(n, a, b)
+        {
+            setValor(v);
+        }
+
+        /**
+         * Metodo abstrato de fontes independents
+         * que determina o valor da fonte
+         * @param v valor da valor
+         */
+        void setValor(double v)
+        {
+            valor = v;
+        }
+};
+
+#endif
