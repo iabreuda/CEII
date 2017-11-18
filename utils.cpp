8f09af2e30a4f27b0857a77a17767fc3ec29376f
Igor Abreu <igor.abreu.da.silva@cern.ch>
2017-11-18 16:23:12 -0300

    Inserir utils para metodo de gauss

--- utils.cpp
+++ utils.cpp
@@ -5,6 +5,20 @@
  *  - istringstream
  */
 #include <sstream>
+ /**
+ * Inclui:
+ *  - vector
+ */
+#include <vector>
+ /**
+ * Inclui:
+ *  - vector
+ */
+#include <vector>
+/**
+ * Tratamento de excecoes
+ */
+#include <stdexcept>
 
 /* Necessario para nao precisar escrever std:: */
 using namespace std;
@@ -27,6 +41,60 @@ const vector<string> explode(const string& phrase, char delim)
     }
 
     return result;
+}
+
+/**
+ * Retorna as tensoes nodais quando voce passa a matriz de condutancia
+ * e a matriz de corrente, aplica a eliminacao de gauss para transformar a matriz
+ * de condutividade em uma matriz identidade.
+ */
+vector<vector<double> > gauss(vector<vector<double> > condutancia, vector<vector<double> > correntes)
+{
+    int condutanciaRows = condutancia.size();
+    int correnteRows = correntes.size();
+
+    /**
+     * Matrizes de condutancia e conrrente devem ter os
+     * mesmos numeros de linhas
+     */
+    if (condutanciaRows != correnteRows) {
+        throw invalid_argument("Matrizes de condutancia e corrente nao tem o msm tamanho");
+    }
+
+    /**
+     * Analisa cada linha da matriz de conduntancias
+     */
+    for(int row = 0; row < condutanciaRows; row++) {
+        double pivot = condutancia[row][row];
+
+        /**
+         * Evita divisao por 0
+         */
+        if (pivot == 0) {
+            pivot = 10^9;
+        }
+
+        int column = condutancia[row].size();
+        if (condutanciaRows != column) {
+            throw invalid_argument("Matrizes de condutancia deve ser quadrada");
+        }
+
+        correntes[row][0] /= pivot;
+        for(int col = 0; col < column; col++) {
+            condutancia[row][col] /= pivot;
+        }
+
+        for(int r = 0; r < condutanciaRows; r++) {
+            if (r != row) {
+                double fator = condutancia[r][row];
+                correntes[r][0] -= correntes[row][0] * fator;
+                for(int c = 0; c < column; c++) {
+                    condutancia[r][c] -= condutancia[row][c] * fator;
+                }
+            }
+        }
+    }
+    return correntes;
 }
 
 #endif
