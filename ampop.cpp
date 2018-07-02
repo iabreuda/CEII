2c6cd9066ef896f1a0100f2b729abaf379693691
Igor Abreu <igor.abreu.da.silva@cern.ch>
2018-07-02 20:43:09 -0300

    Corrigir estampas do ampop

--- ampop.cpp
+++ ampop.cpp
@@ -19,16 +19,7 @@ class AmpOp : public Components4t
         {
         }
 
-        /**
-         * Retorna corrente que auxiliar
-         * para analise nodal modificada
-         */
-        string getAuxNode()
-        {
-            return "j" + getNome();
-        }
-
-        /**
+         /**
          * Estanpa da matriz nodal modificada amp op
          * @param condutancia matriz de condutancia
          * @param correntes   matriz de correntes
@@ -39,14 +30,6 @@ class AmpOp : public Components4t
             vector<string> nodes,
             vector<double> resultado)
         {
-            vector<string>::iterator it;
-            it = find(nodes.begin(), nodes.end(), getAuxNode());
-            auto pos = it - nodes.begin();
-
-            condutancia[getNoA()][pos] += 1;
-            condutancia[getNoB()][pos] += -1;
-            condutancia[pos][getNoC()] += 1;
-            condutancia[pos][getNoD()] += -1;
         }
 };
 
